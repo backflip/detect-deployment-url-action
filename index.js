34f9f6c2432ffa83f700e410b9f99c22045fc857
Thomas Jaggi <thomas@responsive.ch>
2022-04-06 10:44:59 +0200

    feat: add ignoreRegex input

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ try {
   console.log({ context: github.context });
 
   const regex = core.getInput("regex");
+  const ignoreRegex = core.getInput("ignoreRegex");
+
   const { payload } = github.context;
   const comment = payload.comment ? payload.comment.body : null;
 
   console.log({ comment });
 
   if (comment) {
-    const url = comment.match(new RegExp(regex));
+    const ignore = ignoreRegex && !!comment.match(new RegExp(ignoreRegex));
 
-    console.log({ url });
+    if (ignore) {
+      console.log(`Ignoring comment because "ignoreRegex" matched`);
+    } else {
+      const url = comment.match(new RegExp(regex));
 
-    if (url) {
-      core.setOutput("url", url[0]);
+      console.log({ url });
+
+      if (url) {
+        core.setOutput("url", url[0]);
+      }
     }
   }
 } catch (error) {
